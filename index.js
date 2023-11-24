bf580251d8c9644cfbb1ddb77ce6a22e4720e97b
benjaminsalon <benjaminsalon.business@gmail.com>
2023-11-24 18:02:33 +0100

    new version and index small refactoring

--- index.js
+++ index.js
@@ -5,56 +5,62 @@ import { fileURLToPath } from "url";
 import path from "path";
 import inquirer from 'inquirer';
 
-const projectDir = "./soroban-react-dapp/";
+
 const srcDir = path.join(fileURLToPath(path.dirname(import.meta.url)),"soroban-react-dapp");
 const main = async () => {
-    console.log("------------------------------ Welcome soroban dev! ----------------------------");
+    console.log("--------------------------------------- Welcome soroban dev! -------------------------------------");
     console.log("\nYou are creating a new soroban dapp. We wish you best of luck!\n");
     console.log("\nThe script will be setting the initial boilerplate for you.\n");
-    console.log("--------------------------------------------------------------------------------\n\n");
+    console.log("\nWARNING: This next.js project uses `pnpm`as package manager, please make sure it is installed\n");
+    console.log("--------------------------------------------------------------------------------------------------\n\n");
     inquirer.prompt([
         {
+            name: 'name',
+            message: 'What will be the name of your project?',
+            default: 'my-soroban-dapp'
+        },
+        {
+            name: 'install',
+            message: 'Do you want us to install dependencies for you?',
+            default: 'y'
+        },
+        {
             name: 'continue',
-            message: 'Shall we continue?',
+            message: 'Should we continue and set up the dapp?',
             default: 'y'
         },
       ])
       .then(async answers => {
+            const projectDir = answers.name;
           if (answers.continue == 'y' || answers.continue == 'Y' || answers.continue == 'yes') {
               console.log("")
-              console.log("\nCreation of the 'soroban-react-dapp' folder and initialization of the project\n");
+              console.log(`\nCreation of the ${projectDir} folder and initialization of the project\n`);
               try {
                   await execa("mkdir", [projectDir]);
                 }
                 catch (error) {
-                    console.log("---- ERROR Getting an error with creating the soroban-react-dapp dir... \n\n Maybe the soroban-react-folder is already existing in this directory. Consider removing it.\n\n");
+                    console.log(`---- ERROR Getting an error with creating the soroban-react-dapp dir... \n\n Maybe the ${answers.name} folder is already existing in this directory. Consider removing it.\n\n`);
                 process.exit(1);
             }
             fs.copySync(srcDir, projectDir);
-            inquirer.prompt([
-                {
-                    name: 'install',
-                    message: 'Do you want us to install dependencies for you?',
-                    default: 'y'
-                },
-            ])
-            .then(async answers => {
-                if (answers.install == 'y' || answers.install == 'Y' || answers.install == 'yes') {
-                    console.log("")
-                    console.log("Installing packages with 'yarn'");
-                    await execa("yarn", [], {
-                        cwd: projectDir
-                    });
-                    console.log("\n\nYou can now cd in 'soroban-react-dapp' and run 'yarn dev' to start the app\n");
-                    console.log("\n------------------------------------ ENJOY -----------------------------------\n");
-                }
-                else {
-                    
-                    console.log("\n              Perfect then, we are all set!");
-                    console.log("\n\nYou can now cd in 'soroban-react-dapp' to install dependencies and then 'yarn dev' to launch the app.\n");
-                    console.log("\n------------------------------------ ENJOY -----------------------------------\n");
-                }
-              });
+            
+            if (answers.install == 'y' || answers.install == 'Y' || answers.install == 'yes') {
+                console.log("")
+                console.log("Installing packages with 'pnpm'");
+                const subprocess = execa("pnpm", ["install"], {
+                    cwd: projectDir
+                });
+                subprocess.stdout.pipe(process.stdout)
+                subprocess.stderr.pipe(process.stderr)
+                await subprocess
+                console.log(`\n\nYou can now cd in ${projectDir} and run 'pnpm run dev' to start the app\n`);
+                console.log(`\n------------------------------------ ENJOY -----------------------------------\n`);
+            }
+            else {
+                console.log(`\nPerfect then, we are all set!`);
+                console.log(`\n\nYou can now cd in ${projectDir} and run 'pnpm install' to install dependencies and then 'pnpm run dev' to start the app\n`);
+                console.log(`\n------------------------------------ ENJOY -----------------------------------\n`);
+            }
         }
         else {
             console.log("OK aborting...");
