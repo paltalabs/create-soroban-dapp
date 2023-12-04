dacf70cbf7f4a630ef37a604f58969cf7a2713cf
benjaminsalon <benjaminsalon.business@gmail.com>
2023-12-04 19:23:44 +0100

    more verbose

--- soroban-react-dapp/contracts/deploy_on_testnet.sh
+++ soroban-react-dapp/contracts/deploy_on_testnet.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+echo -e "\n\n"
+echo "Using Makefile to build contracts wasm blobs"
+
+echo -e "\n\n"
+
+make
+
+echo -e "\n\n"
 
 echo "Configuring soroban for testnet\n"
 soroban config network add --global testnet \
@@ -8,10 +16,16 @@ soroban config network add --global testnet \
 echo "Configuring deployer identity"
 soroban config identity generate deployer
 
+echo -e "\n\n"
+
 echo "Deployer address is $(soroban config identity address deployer)"
+
+echo -e "\n\n"
 
 echo "Funding deployer address with friendbot"
 curl "https://friendbot.stellar.org/?addr=$(soroban config identity address deployer)"
+
+echo -e "\n\n"
 
 echo "Deploying on testnet"
 CONTRACT_ID="$(soroban contract deploy \
@@ -20,9 +34,13 @@ CONTRACT_ID="$(soroban contract deploy \
   --network testnet)"
 echo "Contract successfully deployed on testnet with contract id $CONTRACT_ID"
 
+echo -e "\n\n"
+
 tmp=$(mktemp) 
 if [[ $(jq ".testnet.$1" contracts_ids.json) ]]; then
     jq ".testnet.$1 = \"$CONTRACT_ID\"" contracts_ids.json > "$tmp" && mv "$tmp" contracts_ids.json
 else
     jq ".testnet += [{\"$1\":\"$CONTRACT_ID\"}]" contracts_ids.json > "$tmp" && mv "$tmp" contracts_ids.json
 fi
+
+echo -e "Check the address of the deployed contracts in contracts_ids.json:\n`cat contracts_ids.json`"
