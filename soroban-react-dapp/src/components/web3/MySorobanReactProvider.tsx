c3f545f594fdf45604893e8377d42d94069ef336
benjaminsalon <benjaminsalon.business@gmail.com>
2024-01-26 18:35:38 +0100

    come back to @

--- soroban-react-dapp/src/components/web3/MySorobanReactProvider.tsx
+++ soroban-react-dapp/src/components/web3/MySorobanReactProvider.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {SorobanReactProvider} from '../../soroban-react/packages/core/src';
+import {SorobanReactProvider} from '@/soroban-react/packages/core/src';
 import {futurenet, sandbox, standalone,testnet} from '@soroban-react/chains';
 import {freighter} from '@soroban-react/freighter';
 import type {ChainMetadata, Connector} from "@soroban-react/types";
-import { xbull } from '../../soroban-react/packages/xbull/src';
+import { xbull } from '@/soroban-react/packages/xbull/src';
       
 const chains: ChainMetadata[] = [sandbox, standalone, futurenet,testnet];
 const connectors: Connector[] = [freighter(), xbull()]
