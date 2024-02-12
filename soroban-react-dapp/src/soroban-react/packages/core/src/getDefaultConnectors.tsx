094639500f97fffb805dc9fd95affbeb22d92901
benjaminsalon <benjaminsalon.business@gmail.com>
2024-02-12 18:44:46 +0100

    fix build

--- soroban-react-dapp/src/soroban-react/packages/core/src/getDefaultConnectors.tsx
+++ soroban-react-dapp/src/soroban-react/packages/core/src/getDefaultConnectors.tsx
@@ -1,6 +1,6 @@
 import { freighter } from '@soroban-react/freighter'
 import { Connector } from '@soroban-react/types'
-import { xbull } from '../../xbull'
+import { xbull } from '../../xbull/dist'
 
 export const getDefaultConnectors = (): Connector[] => {
   const list: Connector[] = [freighter(),xbull()]
