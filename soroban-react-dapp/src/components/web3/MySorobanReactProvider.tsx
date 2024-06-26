e5cd8827349cec6d09768c93bba4fd51d4021abc
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2024-06-26 18:37:02 -0300

    Integration with hana and lobstr

--- soroban-react-dapp/src/components/web3/MySorobanReactProvider.tsx
+++ soroban-react-dapp/src/components/web3/MySorobanReactProvider.tsx
@@ -4,11 +4,13 @@ import {futurenet, sandbox, standalone,testnet} from '@soroban-react/chains';
 import {freighter} from '@soroban-react/freighter';
 import type {ChainMetadata, Connector} from "@soroban-react/types";
 import { xbull } from '@soroban-react/xbull';
+import { hana } from '@soroban-react/hana';
+import { lobstr } from '@soroban-react/lobstr';
 
-import deployments from '../../../contracts/deployments.json'
+import deployments from '../../../contracts/deployments.json';
 
 const chains: ChainMetadata[] = [sandbox, standalone, futurenet,testnet];
-const connectors: Connector[] = [freighter(), xbull()]
+const connectors: Connector[] = [freighter(), xbull(), hana(), lobstr()];
 
 
 export default function MySorobanReactProvider({children}:{children: React.ReactNode}) {
