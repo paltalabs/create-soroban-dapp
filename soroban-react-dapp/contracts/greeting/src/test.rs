1b11f086c2439a82589661472db851bd307dea6e
Tomás Opazo <tomas.opazoasd@gmail.com>
2024-08-19 18:35:49 -0400

    feat(contracts/greeting): Add e2e test for TitleContract

--- soroban-react-dapp/contracts/greeting/src/test.rs
+++ soroban-react-dapp/contracts/greeting/src/test.rs
@@ -1,21 +1,42 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{Env, String};
-
+use soroban_sdk::{testutils::Address as _, Env, String};
 
 #[test]
-fn test() {
+fn e2e() {
     let env = Env::default();
+    env.mock_all_auths();
     let contract_id = env.register_contract(None, TitleContract);
     let client = TitleContractClient::new(&env, &contract_id);
 
-    let client_default_title = client.read_title(); 
-    assert_eq!(client_default_title, String::from_slice(&env, "Default Title"));
+    let default_title = String::from_str(&env, "Paltalabs Challenge");
+    let default_authorized_users: Map<Address, bool> = Map::new(&env);
 
-    client.set_title(&String::from_slice(&env, "My New Title"));
-    let client_new_title = client.read_title(); 
+    let admin: Address = Address::generate(&env);
+    let authorized_user: Address = Address::generate(&env);
 
-    assert_eq!(client_new_title, String::from_slice(&env, "My New Title"));
+    // test default values
+    assert_eq!(client.get_title(), default_title);
+    assert_eq!(client.get_authorized_users(), default_authorized_users);
 
+    // first we need to add an admin user
+    client.set_admin(&admin);
+
+    assert_eq!(client.get_admin(), admin);
+
+    // then we can add authorized users
+    client.set_authorized_user(&authorized_user);
+
+    let mut authorized_users = Map::new(&env);
+    authorized_users.set(authorized_user.clone(), true);
+
+    assert_eq!(client.get_authorized_users(), authorized_users);
+
+    // test setting the title as an authorized user (it works with admin too)
+    let new_title = String::from_str(&env, "Tomás Opazo - Paltalabs Dev :)");
+
+    client.set_title(&authorized_user, &new_title);
+
+    assert_eq!(client.get_title(), new_title);
 }
