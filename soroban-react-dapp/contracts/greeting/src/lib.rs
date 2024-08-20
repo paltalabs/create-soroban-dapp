0a37640ea024a8b2e078887577858cd832267983
elgozaru <eli.gonzalez@protonmail.com>
2024-08-20 21:22:40 +0100

    Submitting check_auth solution for title change (multisigners required version)

--- soroban-react-dapp/contracts/greeting/src/lib.rs
+++ soroban-react-dapp/contracts/greeting/src/lib.rs
@@ -1,24 +1,140 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Env, Symbol, symbol_short, String};
-
-const TITLE: Symbol = symbol_short!("TITLE");
-
+use soroban_sdk::{
+	auth::{Context, CustomAccountInterface}, contract, contracterror,
+	contractimpl, contracttype, symbol_short, Address,
+	BytesN, Env, Symbol, Vec,
+};
 
 #[contract]
-pub struct TitleContract;
+struct AccountContract;
+
+#[contracttype]
+#[derive(Clone)]
+pub struct Signature {
+	pub public_key: BytesN<32>,
+	pub signature: BytesN<64>,
+}
+
+#[contracttype]
+#[derive(Clone)]
+enum DataKey {
+	SignerCnt,
+	Signer(BytesN<32>),
+}
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum AccError {
+	NotEnoughSigners = 1,
+	NegativeAmount = 2,
+	BadSignatureOrder = 3,
+	UnknownSigner = 4,
+	InvalidContext = 5,
+}
+
+const CHANGE_TITLE_FN: Symbol = symbol_short!("set_title");
 
 #[contractimpl]
-impl TitleContract {
+impl AccountContract {
+	pub fn init(env: Env, signers: Vec<BytesN<32>>) {
+		for signer in signers.iter() {
+			env.storage().instance().set(&DataKey::Signer(signer), &());
+		}
+		env.storage()
+			.instance()
+			.set(&DataKey::SignerCnt, &signers.len());
+	}
+}
 
-    pub fn set_title(env: Env, title: String) {
-                env.storage().instance().set(&TITLE, &title)
-    }
+#[contractimpl]
+impl CustomAccountInterface for AccountContract {
+type Error = AccError;
+type Signature = Vec<Signature>;
 
-    pub fn read_title(env: Env) -> String {
-        env.storage().instance().get(&TITLE)
-            .unwrap_or(String::from_str(&env, "Default Title"))
-    }
-    
-} 
+#[allow(non_snake_case)]
+fn __required_auth(
+	env: Env,
+	signature_payload: BytesN<32>,
+	signatures: Vec<Signature>,
+	auth_context: Vec<Context>,
+	) -> Result<(), AccError> {
+	
+		authenticate(&env, &signature_payload, &signatures)?;
+		let tot_signers: u32 = env
+			.storage()
+			.instance()
+			.get::<_, u32>(&DataKey::SignerCnt)
+			.unwrap();
+		let all_signed = tot_signers == signatures.len();
+		let curr_contract = env.current_contract_address();
+		for context in auth_context.iter() {
+			verify_authorization_policy(
+			&env,
+			&context,
+			&curr_contract,
+			all_signed,
+			)?;
+		}
+		Ok(())
+	}
+}
 
+fn authenticate(
+env: &Env,
+signature_payload: &BytesN<32>,
+signatures: &Vec<Signature>,
+) -> Result<(), AccError> {
+	for i in 0..signatures.len() {
+		let signature = signatures.get_unchecked(i);
+		if i > 0 {
+			let prev_signature = signatures.get_unchecked(i - 1);
+			if prev_signature.public_key >= signature.public_key {
+				return Err(AccError::BadSignatureOrder);
+			}
+		}
+		if !env
+			.storage()
+			.instance()
+			.has(&DataKey::Signer(signature.public_key.clone()))
+		{
+			return Err(AccError::UnknownSigner);
+		}
+		env.crypto().ed25519_verify(
+		&signature.public_key,
+		&signature_payload.clone().into(),
+		&signature.signature,
+		);
+	}
+	Ok(())
+}
+
+fn verify_authorization_policy(
+	env: &Env,
+	context: &Context,
+	curr_contract: &Address,
+	all_signed: bool,
+
+	) -> Result<(), AccError> {
+	
+	let contract_context = match context {
+		Context::Contract(c) => {
+			if &c.contract == curr_contract {
+				if !all_signed {
+					return Err(AccError::NotEnoughSigners);
+				}
+			}
+		}
+
+		Context::CreateContractHostFn(_) => return Err(AccError::InvalidContext),
+	};
+
+	if contract_context.fn_name != CHANGE_TITLE_FN
+	&& contract_context.fn_name != Symbol::new(env, "approve")
+	{
+		return Ok(());
+	}
+
+	Ok(())
+}
 mod test;
--- soroban-react-dapp/contracts/greeting/src/test.rs
+++ soroban-react-dapp/contracts/greeting/src/test.rs
@@ -1,21 +1,145 @@
 #![cfg(test)]
+extern crate std;
 
-use super::*;
-use soroban_sdk::{Env, String};
+use ed25519_dalek::Keypair;
+use ed25519_dalek::Signer;
+use rand::thread_rng;
+use soroban_sdk::auth::ContractContext;
+use soroban_sdk::symbol_short;
+use soroban_sdk::testutils::Address as _;
+use soroban_sdk::testutils::AuthorizedFunction;
+use soroban_sdk::testutils::AuthorizedInvocation;
+use soroban_sdk::Val;
+use soroban_sdk::{ auth::Context, testutils::BytesN as _, vec, Address, BytesN, Env, IntoVal, Symbol, };
+use soroban_sdk::testutils::Ledger;
+use soroban_sdk::testutils::LedgerInfo;
 
+use crate::AccError;
+use crate::{AccountContract, AccountContractClient, Signature};
+
+fn generate_keypair() -> Keypair {
+	Keypair::generate(&mut thread_rng())
+}
+
+fn signer_public_key(e: &Env, signer: &Keypair) -> BytesN<32> {
+	signer.public.to_bytes().into_val(e)
+}
+
+fn create_account_contract(e: &Env) -> AccountContractClient {
+	AccountContractClient::new(e, &e.register_contract(None,
+	AccountContract {}))
+}
+
+fn sign(e: &Env, signer: &Keypair, payload: &BytesN<32>) -> Val {
+	Signature {
+		public_key: signer_public_key(e, signer),
+		signature: signer
+			.sign(payload.to_array().as_slice())
+			.to_bytes()
+			.into_val(e),
+	}
+	.into_val(e)
+}
+
+fn token_auth_context(e: &Env, token_id: &Address, fn_name: Symbol, amount: i128) -> Context {
+	Context::Contract(ContractContext {
+		contract: token_id.clone(), fn_name,
+		args: ((), (), amount).into_val(e),
+	})
+}
 
 #[test]
-fn test() {
-    let env = Env::default();
-    let contract_id = env.register_contract(None, TitleContract);
-    let client = TitleContractClient::new(&env, &contract_id);
+fn test_token_auth() {
+	let env = Env::default();
+	env.mock_all_auths();
+	let account_contract = create_account_contract(&env);
+	let mut signers = [generate_keypair(), generate_keypair()];
+	
+	if signers[0].public.as_bytes() > signers[1].public.as_bytes() {
+		signers.swap(0, 1);
+	}
+	
+	account_contract.init(&vec![
+	&env,
+	signer_public_key(&env, &signers[0]),
+	signer_public_key(&env, &signers[1]),
+	]);
+	
+	let payload = BytesN::random(&env);
+	let token = Address::generate(&env);
+	env.try_invoke_contract_check_auth::<AccError>(
+		&account_contract.address,
+		&payload,
+		vec![&env, sign(&env, &signers[0], &payload)].into(),
+		&vec![&env, token_auth_context(&env, &token, Symbol::new(&env,"transfer"), 1000), ],
+	)
+	.unwrap();
 
-    let client_default_title = client.read_title(); 
-    assert_eq!(client_default_title, String::from_slice(&env, "Default Title"));
+	env.try_invoke_contract_check_auth::<AccError>(
+		&account_contract.address,
+		&payload,
+		vec![&env, sign(&env, &signers[0], &payload)].into(),
+		&vec![&env, token_auth_context(&env, &token, Symbol::new(&env,"transfer"), 1000), ],
+	)
+	.unwrap();
 
-    client.set_title(&String::from_slice(&env, "My New Title"));
-    let client_new_title = client.read_title(); 
+	// Add a time limit of 1000 seconds for the token.
+	account_contract.set_time(&token, &1000);
+	assert_eq!(
+	env.auths(),
+	std::vec![(
+	account_contract.address.clone(),
+	
+	AuthorizedInvocation {
+		function: AuthorizedFunction::Contract((
+			account_contract.address.clone(),
+			symbol_short!("set_time"),
+			(token.clone(), 1000_u64).into_val(&env),
+		)),
+		sub_invocations: std::vec![]
+	}
+	)]
+	);
 
-    assert_eq!(client_new_title, String::from_slice(&env, "My New Title"));
-
+	// Attempting a transfer within the time limit should fail.
+	env.ledger().set(LedgerInfo {
+		timestamp: 0,
+		protocol_version: 1,
+		sequence_number: 10,
+		network_id: Default::default(),
+		base_reserve: 10,
+		min_temp_entry_ttl: 16,
+		min_persistent_entry_ttl: 16,
+		max_entry_ttl: 100_000,
+	});
+	
+	env.try_invoke_contract_check_auth::<AccError>(
+		&account_contract.address,
+		&payload,
+		vec![&env, sign(&env, &signers[0], &payload)].into(),
+		&vec![&env, token_auth_context(&env, &token, Symbol::new(&env,"transfer"), 1001) ],
+	)
+	.err()
+	.unwrap()
+	.unwrap() == AccError::TimeLimitExceeded;
+	
+	// Simulate passing of time to allow the next transfer.
+	env.ledger().set(LedgerInfo {
+		timestamp: 1000,
+		protocol_version: 1,
+		sequence_number: 10,
+		network_id: Default::default(),
+		base_reserve: 10,
+		min_temp_entry_ttl: 16,
+		min_persistent_entry_ttl: 16,
+		max_entry_ttl: 100_000,
+	});
+	
+	env.try_invoke_contract_check_auth::<AccError>(
+		&account_contract.address,
+		&payload,
+		vec![&env, sign(&env, &signers[0], &payload)].into(),
+		&vec![&env, token_auth_context(&env, &token, Symbol::new(&env, "transfer"), 1001), ],
+	)
+	.unwrap();
 }
