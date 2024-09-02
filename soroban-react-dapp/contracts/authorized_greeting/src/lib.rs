059337b77b3c63b27ad2bbfd5673775d752ca00f
Nicolás Poblete <nicopobletem@gmail.com>
2024-09-02 07:01:02 -0400

    Better defined Rust Errors

--- soroban-react-dapp/contracts/authorized_greeting/src/lib.rs
+++ soroban-react-dapp/contracts/authorized_greeting/src/lib.rs
@@ -5,47 +5,67 @@ const GREET: Symbol = symbol_short!("GREET");
 const AUTH_GRTRS: Symbol = symbol_short!("AUTH_GTRS");
 const ADMIN: Symbol = symbol_short!("ADMIN");
 
+#[derive(Debug)]
+pub enum ContractError {
+    AdminNotSet = 100,
+    Unauthorized = 200,
+    GreeterNotAuthorized = 201,
+}
+
+impl From<soroban_sdk::Error> for ContractError {
+    fn from(_error: soroban_sdk::Error) -> Self {
+        ContractError::Unauthorized
+    }
+}
+
+impl From<&ContractError> for soroban_sdk::Error {
+    fn from(error: &ContractError) -> soroban_sdk::Error {
+        let code = match error {
+            ContractError::AdminNotSet => soroban_sdk::xdr::ScErrorCode::MissingValue,
+            ContractError::Unauthorized => soroban_sdk::xdr::ScErrorCode::InternalError,
+            ContractError::GreeterNotAuthorized => soroban_sdk::xdr::ScErrorCode::InvalidInput,
+        };
+        soroban_sdk::Error::from_type_and_code(soroban_sdk::xdr::ScErrorType::Contract, code)
+    }
+}
+
 #[contract]
 pub struct AuthorizedGreetingContract;
 
 #[contractimpl]
 impl AuthorizedGreetingContract {
     pub fn set_admin(env: Env, admin: Address) {
-        // Specify the key type as `Symbol` and value type as `Address`
         match env.storage().instance().get::<Symbol, Address>(&ADMIN) {
             Some(current_admin) => {
                 current_admin.require_auth();
             }
-            None => {
-                // If no admin is set, this is the first initialization
-                // No authorization check is needed
-            }
+            None => {}
         }
-
         env.storage().instance().set(&ADMIN, &admin);
     }
 
-    pub fn get_admin(env: &Env) -> Address {
+    pub fn get_admin(env: &Env) -> Result<Address, ContractError> {
         env.storage()
             .instance()
-            .get(&ADMIN)
-            .expect("Admin not set in storage")
+            .get::<Symbol, Address>(&ADMIN)
+            .ok_or(ContractError::AdminNotSet)
     }
 
-    pub fn add_greeter(env: Env, greeter: Address) {
-        let admin = Self::get_admin(&env);  
+    pub fn add_greeter(env: Env, greeter: Address) -> Result<(), ContractError> {
+        let admin = Self::get_admin(&env)?; // Use `?` to propagate the error if any
         admin.require_auth();
 
         let mut authorized_greeter_list = Self::get_authorized_greeter_list(&env);
         authorized_greeter_list.set(greeter, true);
         env.storage().instance().set(&AUTH_GRTRS, &authorized_greeter_list);
+        Ok(())
     }
 
     pub fn get_authorized_greeter_list(env: &Env) -> Map<Address, bool> {
         env.storage().instance().get(&AUTH_GRTRS).unwrap_or(Map::new(env))
     }
 
-    pub fn set_greet(env: Env, greeter: Address, greet: String) {
+    pub fn set_greet(env: Env, greeter: Address, greet: String) -> Result<(), ContractError> {
         greeter.require_auth();
 
         let authorized_greeter_list = Self::get_authorized_greeter_list(&env);
@@ -53,6 +73,9 @@ impl AuthorizedGreetingContract {
 
         if greeter_is_authorized {
             env.storage().instance().set(&GREET, &greet);
+            Ok(())
+        } else {
+            Err(ContractError::GreeterNotAuthorized)
         }
     }
 
